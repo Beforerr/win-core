af1b606007783c05941acd9deff702d2830ec35a
zijin <zzj956959688@gmail.com>
2021-01-26 12:28:57 +0800

    Update GitConfig.ps1

--- GitConfig.ps1
+++ GitConfig.ps1
@@ -2,21 +2,22 @@ $userEmail = "zzj956959688@gmail.com"
 
 git config --global user.name "zijin"
 git config --global user.email $userEmail
+git config color.ui true
 
 ssh-keygen -t rsa -C $userEmail
 
-
-$repositoryName = "win-core"
-cd ~\Desktop
-mkdir $repositoryName
-cd $repositoryName
-echo $repositoryName >> README.md
-git init
-git add *
-git commit -m "Init git repository"
-git branch -M main
-git remote add origin git@github.com:Zijin-Zhang/win-core.git
-git push -u origin main
+git clone git@github.com:Zijin-Zhang/win-core.git
+# $repositoryName = "win-core"
+# cd ~\Desktop
+# mkdir $repositoryName
+# cd $repositoryName
+# echo $repositoryName >> README.md
+# git init
+# git add *
+# git commit -m "Init git repository"
+# git branch -M main
+# git remote add origin git@github.com:Zijin-Zhang/win-core.git
+# git push -u origin main
 
 
 
