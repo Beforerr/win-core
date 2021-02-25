5e88d6620dc43ce55ba963b1d3bffdb50f75ef5c
zijin <zzj956959688@gmail.com>
2021-02-25 21:58:17 +0800

    001

--- GitConfig.ps1
+++ GitConfig.ps1
@@ -2,13 +2,17 @@ $userEmail = "zzj956959688@gmail.com"
 
 git config --global user.name "zijin"
 git config --global user.email $userEmail
-git config --global http.proxy localhost:10808
+git config --global http.proxy localhost:10809
 git config --global color.ui true
+git config --global alias.b branch
+git config --global alias.c commit
+git config --global alias.ca "commit -all"
 git config --global alias.g pull
 git config --global alias.p push
+git config --global alias.s status
 git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
+
+
 git config --global alias.st status
 '$GIT_USER_NAME = "Zijin-Cheung"' >> $PROFILE
 
