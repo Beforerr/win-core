5eced45b19469026bec8f40267a1294970deea1d
zijin <zzj956959688@gmail.com>
2021-07-10 16:21:52 +0800

    Update

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,50 +1,17 @@
-# cp /etc/apt/sources.list.bak
-# echo '''
-# deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
-# deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-# deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-# ''' | sudo tee /etc/apt/sources.list
-
-
 sudo apt install git curl
 git clone https://github.com/Zijin-Cheung/win-core.git
-
 
 # fish
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/bin/fish
 # [jorgebucaran/fisher: A plugin manager for Fish.](https://github.com/jorgebucaran/fisher)
 curl -sL https://cdn.jsdelivr.net/gh/jorgebucaran/fisher@main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
-
-# Bat
-# [sharkdp/bat: A cat(1) clone with wings.](https://github.com/sharkdp/bat)
-wget https://pd.zwc365.com/seturl/https://github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1_amd64.deb
-sudo dpkg -i bat_0.18.1_amd64.deb
-
-# Docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# System debugging
-# [Byobu](https://www.byobu.org/)
-sudo apt install byobu tree dstat ncdu -y
 ## Speedtest
 # [sivel/speedtest-cli: Command line interface for testing internet bandwidth using speedtest.net](https://github.com/sivel/speedtest-cli)
 pip install speedtest-cli
-
-
-
 
 # pip3 config set global.index-url http://mirrors.aliyun.com/pypi/simple/
 # pip3 config set install.trusted-host mirrors.aliyun.com
@@ -61,8 +28,6 @@ pip install thefuck
 git clone git://github.com/wting/autojump.git
 cd autojump && ./install.py
 echo " if test -f /home/zijin/.autojump/share/autojump/autojump.fish; . /home/zijin/.autojump/share/autojump/autojump.fish; end" > ~/.config/fish/config.fish
-
-
 
 # Snap
 set -U fish_user_paths /snap/bin $fish_user_paths
