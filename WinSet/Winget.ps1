a68726411f1738a909f01b673dd9641bce3f95d3
zijin <zzj956959688@gmail.com>
2021-07-12 20:22:47 +0800

    Update

--- WinSet/Winget.ps1
+++ WinSet/Winget.ps1
@@ -7,3 +7,5 @@ winget install Docker.DockerDesktop
 winget install Microsoft.EdgeDev
 # winget install -e --id Microsoft.VisualStudio.2019.Community
 # winget uninstall Microsoft.VisualStudio.2019.Community
+winget install todoist
+winget install Adobe.AdobeAcrobatReaderDC
