5b3b81d28283f496edcfdfc4f03ce51e5d4fcdf6
zijin <zzj956959688@gmail.com>
2021-07-13 14:48:14 +0800

    Update

---
+++ Linux/fisher.sh
@@ -0,0 +1,3 @@
+# [jorgebucaran/fisher: A plugin manager for Fish.](https://github.com/jorgebucaran/fisher)
+curl -sL https://cdn.jsdelivr.net/gh/jorgebucaran/fisher@main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide
--- Ubuntu.sh
+++ Ubuntu.sh
@@ -6,22 +6,6 @@ sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish
 chsh -s /usr/bin/fish
-# [jorgebucaran/fisher: A plugin manager for Fish.](https://github.com/jorgebucaran/fisher)
-curl -sL https://cdn.jsdelivr.net/gh/jorgebucaran/fisher@main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-## Speedtest
-# [sivel/speedtest-cli: Command line interface for testing internet bandwidth using speedtest.net](https://github.com/sivel/speedtest-cli)
-pip install speedtest-cli
-
-# pip3 config set global.index-url http://mirrors.aliyun.com/pypi/simple/
-# pip3 config set install.trusted-host mirrors.aliyun.com
-
-# Glances
-# [Glances - An Eye on your system](https://nicolargo.github.io/glances/)
-pip install glances
-
-# Fuck
-pip install thefuck
 
 # Autojump
 # [wting/autojump: A cd command that learns - easily navigate directories from the command line](https://github.com/wting/autojump)
